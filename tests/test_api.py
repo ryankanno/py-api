26b127f8d2bef9ed3a13aadbe5ac904d554b8565
Ryan Kanno <ryankanno@localkinegrinds.com>
2013-12-20 19:28:04 -0500

    Added host test

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,15 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from nose.tools import eq_
+from py_api.api import Api
 import unittest
+
+
+class FakeApi(Api):
+
+    HOST = "http://github.com"
+
+    def _host(self):
+        return self.HOST
 
 
 class TestApi(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.api = FakeApi()
 
     def test_api(self):
-        pass
+        eq_(self.api.host, FakeApi.HOST)
 
 # vim: filetype=python
